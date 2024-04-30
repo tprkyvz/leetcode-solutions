211b3e94e57158e77a935f2bf569a0f05a180718
ToprakYavuz <111892647+tprkyvz@users.noreply.github.com>
2024-05-01 00:57:55 +0300

    Add files via upload

---
+++ contains-dublicate/contains-dublicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashset = new HashSet<>();
+        for(int num : nums){
+            if(!hashset.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
